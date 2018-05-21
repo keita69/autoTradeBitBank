2acee77c774b087088f51a4c3b4b8b025f3723d3
sawada-keita <sawada.keita@jp.fujitsu.com>
2018-05-21 12:19:45 +0900

    テストコードでLINEメッセージテスト

--- src/test_bitbankAutoOrder.py
+++ src/test_bitbankAutoOrder.py
@@ -1,1 +1,20 @@
-print("HelloWorld")
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pytest
+import time
+import bitbankAutoOrder
+
+@pytest.fixture(scope="module", autouse=True)
+def before_after():
+    # 前処理
+    print "created!"
+    ao = bitbankAutoOrder.AutoOrder()
+    yield ao
+
+    # 後処理
+    print "closed!"
+
+def test_1(before_after):
+    ao.notify_line("LINEメッセージテスト")
+    ao.notify_line("LINEメッセージテスト", "2", "179")
