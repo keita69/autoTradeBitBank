83e72fa0d994cb8c8d0846840595cda1b659255e
keita69sawada <keita69sawada@gmail.com>
2018-05-21 12:38:16 +0900

    テストコードのバグ修正

--- src/test_bitbankAutoOrder.py
+++ src/test_bitbankAutoOrder.py
@@ -5,16 +5,16 @@ import pytest
 import time
 import bitbankAutoOrder
 
+
 @pytest.fixture(scope="module", autouse=True)
 def before_after():
     # 前処理
-    print "created!"
     ao = bitbankAutoOrder.AutoOrder()
     yield ao
-
     # 後処理
-    print "closed!"
+    # nop
+
 
 def test_1(before_after):
-    ao.notify_line("LINEメッセージテスト")
-    ao.notify_line("LINEメッセージテスト", "2", "179")
+    before_after.notify_line("LINEメッセージテスト")
+    before_after.notify_line("LINEメッセージテスト", "2", "179")
