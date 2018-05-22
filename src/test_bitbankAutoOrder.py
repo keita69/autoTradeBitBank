9b51ba5be370bb1ac8df2a58438c6f1720054f99
sawada-keita <sawada.keita@jp.fujitsu.com>
2018-05-22 18:34:12 +0900

    RSIテストコード実装

--- src/test_bitbankAutoOrder.py
+++ src/test_bitbankAutoOrder.py
@@ -11,3 +11,9 @@ def test_notify_line():
 
     http_status = ao.notify_line_stamp("LINEスタンプテスト", "2", "179")
     assert http_status.status_code == 200
+
+
+def test_get_rsi():
+    rsi = get_rsi(self, 14, "1min")
+    assert rsi >= 0
+    assert rsi <= 100
