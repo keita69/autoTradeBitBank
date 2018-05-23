c4c463da27b4fff3079b59ded00132bbbf466e6d
sawada-keita <sawada.keita@jp.fujitsu.com>
2018-05-23 17:42:49 +0900

    mock patch のサンプルを作成

--- src/test_bitbankAutoOrder.py
+++ src/test_bitbankAutoOrder.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-import bitbankAutoOrder
 import python_bitbankcc
+from bitbankAutoOrder import MyTechnicalAnalysisUtil
+from bitbankAutoOrder import AutoOrder
+
+
+def test_patch_get_xrp_jpy_value(monkeypatch):
+    """ [Mock Patch]
+    参考：http://thinkami.hatenablog.com/entry/2017/03/07/065903
+    """
+    last = 50.1
+    sell = 53.1
+    buy = 49.2
+    monkeypatch.setattr(AutoOrder, 'get_xrp_jpy_value',
+                        lambda x: (last, sell, buy))
+    sut = AutoOrder()
+    last, sell, buy = sut.get_xrp_jpy_value()
+    assert (last, sell, buy) == (50.1, 53.1, 49.2)
 
 
 def test_notify_line():
-    ao = bitbankAutoOrder.AutoOrder()
+    ao = AutoOrder()
     http_status = ao.notify_line("LINEメッセージテスト")
     assert http_status.status_code == 200
 
@@ -15,7 +30,18 @@ def test_notify_line():
 
 
 def test_get_rsi():
-    mtau = bitbankAutoOrder.MyTechnicalAnalysisUtil()
+    mtau = MyTechnicalAnalysisUtil()
     rsi = mtau.get_rsi(14, "1min")
     assert rsi >= 0
     assert rsi <= 100
+
+
+def test_get_xrp_jpy_value():
+    ao = AutoOrder()
+    last, sell, buy = ao.get_xrp_jpy_value()
+    f_last = float(last) 
+    f_sell = float(sell)
+    f_buy = float(buy)
+    assert (f_last, f_sell, f_buy) > (0.0, 0.0, 0.0)
+
+
