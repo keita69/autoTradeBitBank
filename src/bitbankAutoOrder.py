cefcb3172d27e6ec3ff8dc9b5eb19def8d959c5e
sawada-keita <sawada.keita@jp.fujitsu.com>
2018-06-04 17:25:34 +0900

    デバッグ分追加

--- src/bitbankAutoOrder.py
+++ src/bitbankAutoOrder.py
@@ -492,7 +492,8 @@ if __name__ == '__main__':
             _, _ = ao.sell_order(buy_result)   # 売り注文処理
 
             activeOrders = bitbank.get_active_orders()["orders"]
-            if activeOrders != "":
+            ao.myLogger.debug("デバッグ 注文一覧 {0}".format(activeOrders))
+            if activeOrders != {}:
                 line.notify_line_stamp("売買数が合いません！！！ 注文数：{0}".format(
                     len(activeOrders)), "1", "422")
                 ao.myLogger.debug("売買数が合いません！！！ 注文数：{0}".format(
