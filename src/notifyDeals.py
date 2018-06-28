4c2e613173126ff92a6199bd09058b5056320386
keita69sawada <keita69sawada@gmail.com>
2018-06-28 23:38:39 +0900

    NASDAQ 100 を追加

--- src/notifyDeals.py
+++ src/notifyDeals.py
@@ -3,12 +3,34 @@ from datetime import datetime
 import pandas as pd
 import pandas_datareader.data as web
 
-NY_DOW_SYMBOLS = ("AAPL", "AXP", "BA", "CAT", "CSCO",
+NY_DOW_SYMBOLS = {"AAPL", "AXP", "BA", "CAT", "CSCO",
                   "CVX", "DIS", "DWDP", "GS", "HD",
                   "IBM", "INTC", "JNJ", "JPM", "KO",
                   "MCD", "MMM", "MRK", "MSFT", "NKE",
                   "PFE", "PG", "TRV", "UNH", "UTX",
-                  "V", "VZ", "WBA", "WMT", "XOM")
+                  "V", "VZ", "WBA", "WMT", "XOM"}
+
+NASDAQ_100_SYMBOLS = {"AAL", "AAPL", "ADBE", "ADI", "ADP",
+                      "ADSK", "ALGN", "ALXN", "AMAT", "AMGN",
+                      "AMZN", "ASML", "ATVI", "AVGO", "BIDU",
+                      "BIIB", "BKNG", "BMRN", "CA", "CDNS",
+                      "CELG", "CERN", "CHKP", "CHTR", "CMCSA",
+                      "COST", "CSCO", "CSX", "CTAS", "CTRP",
+                      "CTSH", "CTXS", "DISH", "DLTR", "EA",
+                      "EBAY", "ESRX", "EXPE", "FAST", "FB",
+                      "FISV", "FOX", "FOXA", "GILD", "GOOG",
+                      "GOOGL", "HAS", "HOLX", "HSIC", "IDXX",
+                      "ILMN", "INCY", "INTC", "INTU", "ISRG",
+                      "JBHT", "JD", "KHC", "KLAC", "LBTYA",
+                      "LBTYK", "LRCX", "MAR", "MCHP", "MDLZ",
+                      "MELI", "MNST", "MSFT", "MU", "MXIM",
+                      "MYL", "NFLX", "NTES", "NVDA", "ORLY",
+                      "PAYX", "PCAR", "PYPL", "QCOM", "QRTEA",
+                      "REGN", "ROST", "SBUX", "SHPG", "SIRI",
+                      "SNPS", "STX", "SWKS", "SYMC", "TMUS",
+                      "TSLA", "TTWO", "TXN", "ULTA", "VOD",
+                      "VRSK", "VRTX", "WBA", "WDAY", "WDC",
+                      "WYNN", "XLNX", "XRAY"}
 
 
 class Rakuten():
